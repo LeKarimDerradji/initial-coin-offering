cbaea1565dac104353605b39d48f5c6b4934b05f
KarimDerradji <karim.derradji.pro@gmail.com>
2021-05-27 17:23:29 +0200

    bequeath_test

--- contracts/Testament.sol
+++ contracts/Testament.sol
@@ -13,34 +13,37 @@ contract Testament {
     address private _doctor;
     bool private _contractEnd;
     mapping (address => uint256) private _legacy;
-    
-    constructor (address owner_,address doctor_){
-        require(owner_ != doctor_, "Testament: You cannot define the owner and the doctor as the same person.");
-        _owner = owner_;
-        _doctor = doctor_;
-    }
-    
+
     //event
     event Bequeath(address indexed account, uint256 amount);
     event DoctorChanged(address indexed doctor);
     event ContractEnded(address doctor);
     event LegacyWithdrew(address indexed account, uint256 amount);
     
+    
+    constructor (address owner_, address doctor_){
+        require(owner_ != doctor_, 'Testament: You cannot define the owner and the doctor as the same person.');
+        _owner = owner_;
+        _doctor = doctor_;
+    }
+    
+    
     // modifier
     modifier onlyOwner() {
-        require (msg.sender == _owner, "Testament: You are not allowed to use this function.");
+        require (msg.sender == _owner, 'Testament:  only the owner is allow to call that function.');
         _;
     }
     
     modifier onlyDoctor() {
-        require(msg.sender == _doctor, "Testament: You are not allowed to use this function.");
+        require(msg.sender == _doctor, 'Testament: only the doctor is allow to call that function.');
         _;
     }
     
     modifier contractOver() {
-        require(_contractEnd == true,"Testament: The contract has not yet over.");
+        require(_contractEnd == true,'Testament: The contract has not yet over.');
         _;
     }
+    
     
     // functions
     function bequeath(address account) external payable onlyOwner {
@@ -49,25 +52,25 @@ contract Testament {
     }
     
     function setDoctor(address account) public onlyOwner {
-        require(msg.sender != account, "Testament: You cannot be set as doctor.");
+        require(msg.sender != account, 'Testament: You cannot be set as doctor.');
         _doctor = account;
         emit DoctorChanged(account);
     }
     
     function contractEnd() public onlyDoctor {
-        require(_contractEnd == false,"Testament: The contract is already over.");
+        require(_contractEnd == false,'Testament: The contract is already over.');
         _contractEnd = true;
         emit ContractEnded(msg.sender);
     }
     
     function withdraw() public contractOver {
-        require(_legacy[msg.sender] != 0,"Testament: You do not have any legacy on this contract.");
+        require(_legacy[msg.sender] != 0,'Testament: You do not have any legacy on this contract.');
         uint256 amount = _legacy[msg.sender];
         _legacy[msg.sender] = 0;
         payable(msg.sender).sendValue(amount);
         emit LegacyWithdrew(msg.sender,amount);
     }
-    
+
     function legacyOf(address account) public view returns (uint256) {
         return _legacy[account];
     }
@@ -83,6 +86,7 @@ contract Testament {
     function isContractOver() public view returns (bool) {
         return _contractEnd;
     }
+    
 }
 
 
--- test/testament-test.js
+++ test/testament-test.js
@@ -1,11 +1,39 @@
-const {expect} = import('chai')
+const {expect} = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Testament', function ()  {
-    let dev, owner, doctor, recipient, Testament, testament;
-    beforeEach(async function () {
-        [owner, doctor, recipient] = await ethers.getSigners();
-        Testament = await ethers.getContractFactory('Testament');
-        testament = await Testament.connect(dev).deploy(owner.address)
-    })
+  let dev, owner, doctor, Testament, testament;
+  beforeEach(async function () {
+      [dev, owner, doctor, alice] = await ethers.getSigners();
+      Testament = await ethers.getContractFactory('Testament');
+      testament = await Testament.connect(dev).deploy(owner.address, doctor.address);
+      testament.deployed();
+  });
+
+// 
+describe('Deployement', function () {
+    it('Doctor should not be owner', async function () {
+      await expect(Testament.connect(dev).deploy(doctor.address, doctor.address))
+     .to.be.revertedWith('Testament: You cannot define the owner and the doctor as the same person.');
+    });
+  });
+
+describe('Bequeath', function( ) {
+  it('Legacy account should update', async function () {
+    await testament.connect(owner).bequeath(alice.address, {value: 1000});
+    expect(await testament.connect(alice).legacyOf(alice.address)).to.equal(1000);
+  });
+  it('Should emit Bequeath event', async function () {
+    await expect(testament.connect(owner).bequeath(alice.address, {value : 1000}))
+    .to.emit(testament, 'Bequeath')
+    .withArgs(alice.address, 1000);
+  });
+  it('Sould revert if other than owner is triyng to call the function', async function () {
+    await expect(testament.connect(doctor).bequeath(alice.address, {value : 1000}))
+    .to.revertedWith('Testament:  only the owner is allow to call that function.')
+
+  })
+
+});
 
 });
